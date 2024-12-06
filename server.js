ee42e8035f5207f19909d8d511a9494c314c1241
Milena Leticia <milenaleticia.15sa@gmail.com>
2024-12-06 20:58:29 -0300

    Com o server atualizado

--- server.js
+++ server.js
@@ -1,184 +1,159 @@
-window.onload = () => {
-    adicionarFilme();
-    inforFilmesAdmin();
-    cancelar();
-};
-function adicionarFilme() {
-    const adicionarFilmeButton = document.getElementById('adicionarButton');
-    const cancelarButton = document.getElementById('cancelar');
-    const formulario = document.querySelector('form.filmes');
-    const containerFilmes = document.getElementById('filmesContainerAdmin');
-    const cadastrarButton = document.getElementById('cadastrar');
-    const salvarAtualizacaoButton = document.getElementById('salvarAtualizacao');
-    formulario.style.display = 'none';
-    cancelarButton.style.display = 'none';
-    adicionarFilmeButton.addEventListener('click', () => {
-        formulario.style.display = 'block';
-        cancelarButton.style.display = 'block';
-        adicionarFilmeButton.style.display = 'none';
-        cadastrarButton.style.display = 'block';
-        salvarAtualizacaoButton.style.display = 'none';
-        formulario.reset();
-        document.getElementById('filmeId').value = '';
-        if (containerFilmes) {
-            containerFilmes.style.display = 'none';
-        }
-    });
-    formulario.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const formData = new FormData(formulario);
-        const filmeId = document.getElementById('filmeId').value;
-        const url = filmeId ? `/filme/${filmeId}` : '/salvarFilme';
-        const method = filmeId ? 'PUT' : 'POST';
-        fetch(url, {
-            method: method,
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                alert(data.error); 
-            } else if (data.mensagem) {
-                alert(data.mensagem); 
-            } else {
-                alert('Operação realizada com sucesso!'); 
-            }
-            formulario.reset();
-            formulario.style.display = 'none';
-            cancelarButton.style.display = 'none';
-            adicionarFilmeButton.style.display = 'block';
-            if (containerFilmes) {
-                containerFilmes.style.display = 'flex';
-            }
-            inforFilmesAdmin();
-        })
-        .catch(error => {
-            console.error('Erro ao salvar o filme:', error);
-            alert('Erro ao salvar o filme.');
-        });
-    });
-}
-function cancelar() {
-    const adicionarFilmeButton = document.getElementById('adicionarButton');
-    const cancelarButton = document.getElementById('cancelar');
-    const formulario = document.querySelector('form.filmes');
-    const containerFilmes = document.getElementById('filmesContainerAdmin');
-    cancelarButton.addEventListener('click', () => {
-        formulario.style.display = 'none';
-        cancelarButton.style.display = 'none';
-        adicionarFilmeButton.style.display = 'block';
-        if (containerFilmes) {
-            containerFilmes.style.display = 'flex'; 
-        }
-    });
-}
-function inforFilmesAdmin() {
-    fetch('/filmes')
-        .then(response => response.json())
-        .then(filmes => {
-            const container = document.getElementById('filmesContainerAdmin');
-            container.innerHTML = ''; 
-            filmes.forEach(filme => {
-                const filmeDiv = document.createElement('div');
-                filmeDiv.classList.add('filme-item');
-                filmeDiv.innerHTML = `
-                    <button class="filme-imagem-btn" data-id="${filme.id}" data-nome="${filme.nome}" data-genero="${filme.genero}" data-classificacao="${filme.classificacao}" data-descricao="${filme.descricao}" data-idioma="${filme.idioma}" data-imagem="/uploads/${filme.imagem}">
-                        <img src="/uploads/${filme.imagem}" alt="${filme.nome}">
-                    </button>
-                `;
-                container.appendChild(filmeDiv);
-            });
-            document.querySelectorAll('.filme-imagem-btn').forEach(button => {
-                button.addEventListener('click', (event) => {
-                    const id = event.currentTarget.getAttribute('data-id');
-                    const nome = event.currentTarget.getAttribute('data-nome');
-                    const genero = event.currentTarget.getAttribute('data-genero');
-                    const classificacao = event.currentTarget.getAttribute('data-classificacao');
-                    const descricao = event.currentTarget.getAttribute('data-descricao');
-                    const idioma = event.currentTarget.getAttribute('data-idioma');
-                    const imagem = event.currentTarget.getAttribute('data-imagem');
-                    mostrarInformacoes(id, nome, genero, classificacao, descricao, idioma, imagem);
-                });
-            });
-        })
-        .catch(error => {
-            console.error('Erro ao carregar filmes:', error);
-        });
-}
-function mostrarInformacoes(id, nome, genero, classificacao, descricao, idioma, imagem) {
-    const detalhes = document.getElementById('detalhesFilmeAdmin');
-    const adicionarFilmeButton = document.getElementById('adicionarButton');
-    
-    detalhes.innerHTML = `
-        <img src="${imagem}" alt="${nome}" style="width: 200px; height: auto;">
-        <div>
-            <h3>${nome}</h3>
-            <p>Gênero: ${genero}</p>
-            <p>Classificação: ${classificacao}</p>
-            <p>Descrição: ${descricao}</p>
-            <p>Idioma: ${idioma}</p>
-        </div>
-        <button id="voltarButton">Voltar</button>
-        <input type="button" id="atualizarButton" value="Atualizar">
-        <input type="button" id="excluirButton" value="Excluir">
-    `;
-    detalhes.style.display = 'block';
-    adicionarFilmeButton.style.display = 'none';
-    const containerFilmes = document.getElementById('filmesContainerAdmin');
-    if (containerFilmes) {
-        containerFilmes.style.display = 'none';
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const app = express();
+const port = 3000;
+// Configuração do multer para armazenamento de arquivos
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Pasta onde os arquivos serão armazenados
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); 
     }
-    document.getElementById('voltarButton').addEventListener('click', () => {
-        detalhes.style.display = 'none';
-        adicionarFilmeButton.style.display = 'block';
-        if (containerFilmes) {
-            containerFilmes.style.display = 'flex'; 
+});
+const upload = multer({ storage: storage });
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+if (!fs.existsSync('uploads')){
+    fs.mkdirSync('uploads');
+}
+const arquivoFilmes = path.join(__dirname, 'filme.json');
+app.get('/', (req, res) => {
+    res.redirect('/totem');
+});
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'admin.html'));
+});
+app.get('/totem', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'totem', 'totem.html'));
+});
+app.get('/filmes', (req, res) => {
+    fs.readFile(arquivoFilmes, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo:', err);
+            return res.status(500).send('Erro ao processar dados');
         }
+        let filmes = [];
+        if (data) {
+            filmes = JSON.parse(data);
+        }
+        res.json(filmes);
     });
-    document.getElementById('atualizarButton').addEventListener('click', () => {
-        preencherFormularioParaAtualizacao(id, nome, genero, classificacao, descricao, idioma, imagem);
+});
+app.post('/salvarFilme', upload.single('imagemFilme'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).send('Erro: Imagem do filme é obrigatória.');
+    }
+    const novoFilme = {
+        id: req.file.filename,
+        nome: req.body.nomeFilme,
+        genero: req.body.generoFilme,
+        classificacao: req.body.classificacaoFilme,
+        descricao: req.body.descricaoFilme,
+        idioma: req.body.idiomaFilme,
+        imagem: req.file.filename
+    };
+    fs.readFile(arquivoFilmes, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo:', err);
+            return res.status(500).send('Erro ao processar dados');
+        }
+        let filmes = [];
+        if (data) {
+            filmes = JSON.parse(data);
+        }
+        filmes.push(novoFilme);
+        fs.writeFile(arquivoFilmes, JSON.stringify(filmes, null, 2), 'utf8', (err) => {
+            if (err) {
+                console.error('Erro ao salvar os dados:', err);
+                return res.status(500).send('Erro ao salvar os dados.');
+            }
+            console.log('Filme cadastrado com sucesso!');
+            res.redirect('/totem'); // Redireciona para o totem após o cadastro
+        });
     });
-    document.getElementById('excluirButton').addEventListener('click', () => {
-        if (confirm('Você tem certeza que deseja excluir este filme?')) {
-            fetch(`/filme/${id}`, {
-                method: 'DELETE'
-            })
-            .then(response => response.text())
-            .then(message => {
-                alert(message);
-                inforFilmesAdmin(); // Atualiza a lista de filmes
-                detalhes.style.display = 'none';
-                adicionarFilmeButton.style.display = 'block';
-                if (containerFilmes) {
-                    containerFilmes.style.display = 'flex'; 
+});
+app.put('/filme/:id', upload.single('imagemFilme'), (req, res) => {
+    fs.readFile(arquivoFilmes, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo:', err);
+            return res.status(500).send('Erro no servidor.');
+        }
+        let filmes = JSON.parse(data);
+        const filmeId = req.params.id;
+        let filmeIndex = filmes.findIndex(f => f.id == filmeId);
+
+        if (filmeIndex !== -1) {
+            const novaImagem = req.file ? req.file.filename : null;
+            const novoId = novaImagem || filmeId;
+            filmes[filmeIndex] = {
+                id: novoId,
+                nome: req.body.nomeFilme,
+                genero: req.body.generoFilme,
+                classificacao: req.body.classificacaoFilme,
+                descricao: req.body.descricaoFilme,
+                idioma: req.body.idiomaFilme,
+                imagem: novaImagem || filmes[filmeIndex].imagem
+            };
+            fs.writeFile(arquivoFilmes, JSON.stringify(filmes, null, 2), err => {
+                if (err) {
+                    console.error('Erro ao escrever no arquivo:', err);
+                    return res.status(500).send('Erro no servidor.');
                 }
-            })
-            .catch(error => {
-                console.error('Erro ao excluir o filme:', error);
-                alert('Erro ao excluir o filme.');
+                res.send('Filme atualizado com sucesso.');
             });
+        } else {
+            res.status(404).send('Filme não encontrado.');
         }
     });
-}
-function preencherFormularioParaAtualizacao(id, nome, genero, classificacao, descricao, idioma, imagem) {
-    const formulario = document.querySelector('form.filmes');
-    const cancelarButton = document.getElementById('cancelar');
-    const cadastrarButton = document.getElementById('cadastrar');
-    const salvarButton = document.getElementById('salvarAtualizacao');
-    document.getElementById('filmeId').value = id;
-    document.getElementById('nomeFilme').value = nome;
-    document.getElementById('generoFilme').value = genero;
-    document.getElementById('classificacaoFilme').value = classificacao;
-    document.getElementById('descricaoFilme').value = descricao;
-    document.getElementById('idiomaFilme').value = idioma;
-    formulario.style.display = 'block';
-    cancelarButton.style.display = 'block';
-    cadastrarButton.style.display = 'none'; 
-    salvarButton.style.display = 'block'; 
-    const adicionarFilmeButton = document.getElementById('adicionarButton');
-    adicionarFilmeButton.style.display = 'none';
-    const containerFilmes = document.getElementById('filmesContainerAdmin');
-    if (containerFilmes) {
-        containerFilmes.style.display = 'none';
+});
+app.delete('/filme/:id', (req, res) => {
+    const filmeId = req.params.id;
+    console.log(`Recebido pedido de exclusão para o filme com ID: ${filmeId}`);
+
+    if (!filmeId) {
+        return res.status(400).send('ID do filme não fornecido.');
     }
-}
+
+    fs.readFile(arquivoFilmes, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo:', err);
+            return res.status(500).send('Erro no servidor.');
+        }
+
+        let filmes = JSON.parse(data);
+        const filme = filmes.find(f => f.id === filmeId);
+        if (!filme) {
+            console.log(`Filme com ID: ${filmeId} não encontrado.`);
+            return res.status(404).send('Filme não encontrado.');
+        }
+
+        filmes = filmes.filter(f => f.id !== filmeId);
+        const caminhoImagem = path.join(__dirname, 'uploads', filme.imagem);
+
+        fs.unlink(caminhoImagem, (err) => {
+            if (err) {
+                console.error('Erro ao remover a imagem:', err);
+                return res.status(500).send('Erro ao remover a imagem do servidor.');
+            }
+            console.log(`Imagem do filme com ID: ${filmeId} removida com sucesso.`);
+
+            fs.writeFile(arquivoFilmes, JSON.stringify(filmes, null, 2), err => {
+                if (err) {
+                    console.error('Erro ao escrever no arquivo:', err);
+                    return res.status(500).send('Erro no servidor.');
+                }
+                console.log(`Filme com ID: ${filmeId} excluído com sucesso.`);
+                res.send('Filme excluído com sucesso.');
+            });
+        });
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
